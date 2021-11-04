8b206f1fb7dc325ecc1da42bbcee5cb06a18d2c3
aleksander <a.zaitsev@softaria.com>
2021-11-04 23:17:23 +0700

    backend shared initialization

---
+++ backend-shared/src/test/java/ru/nsu/backendshared/BackendSharedApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.nsu.backendshared;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSharedApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
